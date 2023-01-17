747a69b7d174e668fa8205e2e775cc19d4c69dca
gryder8 <gavin.ryder82@icloud.com>
2023-01-16 21:39:31 -0800

    Initial Commit

---
+++ OpenAI Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAI Client
+//
+//  Created by Gavin Ryder on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
